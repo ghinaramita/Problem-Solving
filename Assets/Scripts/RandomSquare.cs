53c3e752adf9d1e6c24631e160cbc79c64f23441
Ghina Aramita <ghinaramita@gmail.com>
2021-10-13 00:38:02 +0700

    Add Random Square

---
+++ Assets/Scripts/RandomSquare.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSquare : MonoBehaviour
+{
+	public Transform[] Positions;
+	public GameObject Object;
+	public Transform Location;
+	public bool ToSpawn = true;
+
+	void Update()
+	{
+		Location = Positions[Random.Range(0, Positions.Length)];
+
+		if (ToSpawn == true)
+		{
+			Instantiate(Object, Location);
+			ToSpawn = false;
+			StartCoroutine(ToSpawnTrue());
+		}
+	}
+
+	IEnumerator ToSpawnTrue()
+	{
+		yield return new WaitForSeconds(0.75f);
+		ToSpawn = true;
+	}
+}
